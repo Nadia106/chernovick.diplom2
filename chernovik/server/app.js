5d7ae22dc30962ab1ab80f1fef247317ca3eab34
Nadia106 <95348769+Nadia106@users.noreply.github.com>
2022-05-03 00:37:21 +0500

    Update app.js

--- chernovik/server/app.js
+++ chernovik/server/app.js
@@ -10,8 +10,9 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/api", routes);
 app.use(cors());
+app.use("/api", routes);
+
 
 const PORT = config.get("port") ?? 8080;
 
